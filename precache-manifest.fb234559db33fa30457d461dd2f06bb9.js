7406040e28602b5e4a3f8d183baf6b1fdb57f6db
unknown <imonhasans33@gmail.com>
2020-10-04 16:51:36 +0600

    Updates

---
+++ precache-manifest.fb234559db33fa30457d461dd2f06bb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9e969ec99c5c5ad9da11709643780b",
+    "url": "/Expense-Tracker/index.html"
+  },
+  {
+    "revision": "f4ffef64ba162601404b",
+    "url": "/Expense-Tracker/static/css/main.4df4a2b6.chunk.css"
+  },
+  {
+    "revision": "fdb5cfeca250a7e2946a",
+    "url": "/Expense-Tracker/static/js/2.bd509c3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expense-Tracker/static/js/2.bd509c3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ffef64ba162601404b",
+    "url": "/Expense-Tracker/static/js/main.a4a1cb98.chunk.js"
+  },
+  {
+    "revision": "f5cbc41e0a6f2411c541",
+    "url": "/Expense-Tracker/static/js/runtime-main.95e54716.js"
+  }
+]);
